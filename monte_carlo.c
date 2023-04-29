98ce44a80a6b115fbf361561e5903af974caf404
shpc136 <ch950410@gmail.com>
2023-04-29 17:59:22 +0900

    second commit

--- monte_carlo.c
+++ monte_carlo.c
@@ -1,29 +1,70 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
 
 #include "monte_carlo.h"
 #include "util.h"
 
-double monte_carlo(double *xs, double *ys, int num_points, int mpi_rank, int mpi_world_size, int threads_per_process) {
+double monte_carlo(double *xs, double *ys, int num_points, int mpi_rank, int mpi_world_size, int threads_per_process)
+{
   int count = 0;
+  int num_points_per_proc = num_points / mpi_world_size;
 
-  // TODO: Parallelize the code using mpi_world_size processes (1 process per
-  // node.
-  // In total, (mpi_world_size * threads_per_process) threads will collaborate
-  // to compute the Riemann sum.
+  // scatter the points to all processes
+  double *x_buf = (double *)malloc(num_points_per_proc * sizeof(double));
+  double *y_buf = (double *)malloc(num_points_per_proc * sizeof(double));
+  MPI_Scatter(xs, num_points_per_proc, MPI_DOUBLE, x_buf, num_points_per_proc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Scatter(ys, num_points_per_proc, MPI_DOUBLE, y_buf, num_points_per_proc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-  if (mpi_rank == 0) {
-    for (int i = 0; i < num_points; i++) {
-      double x = xs[i];
-      double y = ys[i];
+// set up OpenMP parallel region
+#pragma omp parallel num_threads(threads_per_process)
+  {
+    int i;
+    int local_count = 0;
+    double x, y;
 
-      if (x*x + y*y <= 1)
-        count++;
+// loop over the points assigned to this thread
+#pragma omp for schedule(static)
+    for (i = 0; i < num_points_per_proc; i++)
+    {
+      x = x_buf[i];
+      y = y_buf[i];
+
+      if (x * x + y * y <= 1)
+        local_count++;
     }
+
+// reduce the counts from all threads in this process
+#pragma omp atomic
+    count += local_count;
   }
 
+  // gather the counts from all processes and compute the total count
+  int *counts = NULL;
+  if (mpi_rank == 0)
+  {
+    counts = (int *)malloc(mpi_world_size * sizeof(int));
+  }
+  MPI_Gather(&count, 1, MPI_INT, counts, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  // Rank 0 should return the estimated PI value
-  // Other processes can return any value (don't care)
-  return (double) 4 * count / num_points;
+  // compute the total count and return the estimated PI value
+  if (mpi_rank == 0)
+  {
+    int total_count = 0;
+    for (int i = 0; i < mpi_world_size; i++)
+    {
+      total_count += counts[i];
+    }
+    free(counts);
+    free(x_buf);
+    free(y_buf);
+    return (double)4 * total_count / num_points;
+  }
+  else
+  {
+    free(x_buf);
+    free(y_buf);
+    return 0.0;
+  }
 }
